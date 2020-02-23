18404ba701e6751f51892ab61aa616169e021ffd
leadream <leadream4@gmail.com>
2020-02-23 18:34:47 +0800

    移除 [url]

--- api/[url].js
+++
@@ -1,9 +0,0 @@
-const options = {
-  requireHeader: ['origin', 'x-requested-with'],
-  removeHeaders: ['cookie', 'cookie2']
-};
-const cors_proxy = require('../lib/cors-anywhere').createServer(options);
-
-module.exports = (req, res) => {
-  cors_proxy.emit('request', req, res);
-};
